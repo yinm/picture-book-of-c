48852e36a15af5f40947de26109780605ed7f6d0
yinm <yinm.cminor9@gmail.com>
2017-10-27 21:46:27 +0900

    Add escape sequence

--- ch1/01.c
+++ ch1/01.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
 main()
 {
-  char s[10] = "Hello";
-  printf("%s\n", s);
-  strcpy(s, "Good bye");
-  printf("%s\n", s);
+  printf("%8s   %8s\n", "商品A", "商品B");
+  printf("数量%08d %08d\n", 16, 246);
+  printf("重量%8.4f %8.4f\n", 76.3, 556.1);
+  printf("%d%c", 20, 10);
+  printf("%d\bA\n", 20);
+  printf("%d\t%d\n", 20, 30);
 }
