d669ec6424ed268c477c4edebb1a8093b20958ef
yinm <yinm.cminor9@gmail.com>
2017-10-27 22:00:18 +0900

    Add front-end incremnet and back-end increment

--- ch2/a.c
+++ ch2/a.c
@@ -3,11 +3,11 @@
 main()
 {
   int a = 1;
-  printf("はじめは%dでした。\n", a);
+  int b = 1;
 
-  a++;
-  printf("1増えて%dになりました。\n", a);
+  printf("前置だと%dになります。\n", ++a);
+  printf("もう一度%dを出力しておきます\n", a);
 
-  a--;
-  printf("1減って%dに戻りました。\n", a);
+  printf("後置だと%dになります。\n", b++);
+  printf("もう一度%dを出力しておきます\n", b);
 }
