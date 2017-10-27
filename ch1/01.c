ebd454e051d1615d85364ed1cb12f642e09fba16
yinm <yinm.cminor9@gmail.com>
2017-10-27 21:33:03 +0900

    Add types of number

--- ch1/01.c
+++ ch1/01.c
@@ -2,11 +2,11 @@
 
 main()
 {
-  int a;
-  int b;
-  a = 2;
-  b = 3;
-  a = b;
+  unsigned char age = 25;
+  double height = 166.7;
+  float weight = 58.5;
 
-  printf("%d\n", a);
+  printf("年齢:%d歳\n", age);
+  printf("身長:%fcm\n", height);
+  printf("体重:%fkg\n", weight);
 }
