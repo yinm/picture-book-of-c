be0463338b5874f58bc65c24d410858fa6c0dbf5
yinm <yinm.cminor9@gmail.com>
2017-10-27 21:53:48 +0900

    Add calculating

---
+++ ch2/a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+  printf("5+5は%dです。\n", 5+5);
+  printf("5-5は%dです。\n", 5-5);
+  printf("5*5は%dです。\n", 5*5);
+  printf("5/5は%dです。\n", 5/5);
+  printf("5/3の余りは%dです。\n", 5%3);
+}
