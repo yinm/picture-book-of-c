03d22d8f690a3d7babfb27f26423e6386423cdd3
yinm <yinm.cminor9@gmail.com>
2017-10-27 21:38:18 +0900

    Add type of string

--- ch1/01.c
+++ ch1/01.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 main()
 {
-  char character = 'A';
-  printf("%d\n", character);
-  printf("%c\n", character);
-
-  char number = 65;
-  printf("%d\n", number);
-  printf("%c\n", number);
+  char s[10] = "Hello";
+  printf("%s\n", s);
+  strcpy(s, "Good bye");
+  printf("%s\n", s);
 }
