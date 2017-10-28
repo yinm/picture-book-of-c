8a3e0f01893d4ef4c9baf52f9c6a33610ceb2599
yinm <yinm.cminor9@gmail.com>
2017-10-28 17:54:58 +0900

    Add keyboard inputs

--- ch6/a.c
+++ ch6/a.c
@@ -2,8 +2,23 @@
 
 int main()
 {
-  char s[30];
-  fgets(s, 29, stdin);
-  fputs(s, stdout);
-  fputs("error!\n", stderr);
+  int a;
+  int b = 7;
+  char s[40];
+
+  printf("名前を入力してください\n");
+  gets(s);
+
+  printf("数当てクイズ！ 0から9の数字を入力してね\n");
+  while (a != b) {
+    scanf("%d", &a);
+    if ((a == b-1) || (a == b+1))
+      printf("おしい！\n");
+    else if (a > b+1)
+      printf("もっと小さい数字です\n");
+    else if (a < b-1)
+      printf("もっと大きい数字です\n");
+  }
+
+  printf("正解！ %s さん、おめでとうございます！！\n", s);
 }
