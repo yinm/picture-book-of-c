285a98a8b6bdd3bfa9ee75271f0b033e295615dc
yinm <yinm.cminor9@gmail.com>
2017-10-28 10:25:29 +0900

    Add if4

--- ch3/a.c
+++ ch3/a.c
@@ -2,16 +2,15 @@
 
 main()
 {
-  char c = '#';
+  int a = 90;
 
-  printf("%cは", c);
-
-  if ('0' <= c && c <= '9')
-    printf("数字です.\n");
-  else if('a' <= c && c <= 'z')
-    printf("小文字です.\n");
-  else if('A' <= c && c <= 'Z')
-    printf("大文字です.\n");
+  if (a > 80)
+  {
+    if (a == 100)
+      printf("満点です\n");
+    else
+      printf("もう少しです\n");
+  }
   else
-    printf("記号です.\n");
+    printf("がんばりましょう\n");
 }
