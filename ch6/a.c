eed215b94a1387dac68204a798b8e1874a140df8
yinm <yinm.cminor9@gmail.com>
2017-10-28 17:47:37 +0900

    Add manipulating standard I/O files

--- ch6/a.c
+++ ch6/a.c
@@ -2,31 +2,8 @@
 
 int main()
 {
-  FILE *fp;
-  char filename[] = "bintest2.dat";
-  int buf_w[10]; 
-  int buf_r[10];
-  int i;
-
-  for (i = 0; i < 10; i++)
-    buf_w[i] = (i + 1) * 10;
-  if (! (fp = fopen(filename, "wb")))
-    return 1;
-  if (fwrite(buf_w, sizeof(int), 10, fp) != 10) {
-    fclose(fp);
-    return 1;
-  }
-  fclose(fp);
-
-  if (! (fp = fopen(filename, "rb")))
-    return 1;
-  if (fread(buf_r, sizeof(int), 10, fp) != 10) {
-    fclose(fp);
-    return 1;
-  }
-  fclose(fp);
-
-  for (i = 0; i < 10; i++) {
-    printf("%d ", buf_r[i]);
-  }
+  char s[30];
+  fgets(s, 29, stdin);
+  fputs(s, stdout);
+  fputs("error!\n", stderr);
 }
