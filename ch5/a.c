2eeceb0fe5cb72d85db6128f948567906ea60e34
yinm <yinm.cminor9@gmail.com>
2017-10-28 15:00:06 +0900

    Add year converter

--- ch5/a.c
+++ ch5/a.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
-  char usage[] = "usage: trash <-d> filename\n";
-  int ret = 0;
-  char newfilename[256] = "";
+int wtoj(int, char *, int *);
 
-  if (argc <= 1) {
-    printf(usage);
-    return 1;
+int main()
+{
+  int wyear = -1;
+  int jyear = 0;
+  char nengo[16];
+
+  printf("西暦->和暦の変換を行います。終了するには 0 を入力してください\n");
+  while (wyear != 0) {
+    printf("西暦を入力してください(1868 - 2050) : ");
+    scanf("%d", &wyear);
+    if (wtoj(wyear, nengo, &jyear) == 0)
+      printf("西暦%d年は、 %s%d年です\n", wyear, nengo, jyear);
   }
-  else if(strcmp(argv[1], "-d") == 0) {
-    if (argc <= 2) {
-      printf(usage);
-      return 2;
-    }
-    ret = remove(argv[2]);
-    if (ret == 0)
-      printf("ファイルを削除しました\n");
-    else
-      printf("ファイルを削除できませんでした\n");
-  }
-  else {
-    sprintf(newfilename, "%s.bak", argv[1]);
-    ret = rename(argv[1], newfilename);
-    if (ret == 0)
-      printf("ファイル名の最後に.bakを付加しました\n");
-    else
-      printf("ファイル名の変更ができませんでした\n");
-  }
+  return 0;
 }
 
+int wtoj(int wyear, char *nengo, int *jyear)
+{
+  if (wyear >= 1868 && wyear <= 1911) {
+    strcpy(nengo, "明治");
+    *jyear = wyear - 1868 + 1;
+    return 0;
+  } else if (wyear >= 1912 && wyear <= 1925) {
+    strcpy(nengo, "大正");
+    *jyear = wyear - 1912 + 1;
+    return 0;
+  } else if (wyear >= 1926 && wyear <= 1988) {
+    strcpy(nengo, "昭和");
+    *jyear = wyear - 1926 + 1;
+    return 0;
+  } else if (wyear >= 1989 && wyear <= 2050) {
+    strcpy(nengo, "平成");
+    *jyear = wyear - 1989 + 1;
+    return 0;
+  }
+  return 1;
+}
