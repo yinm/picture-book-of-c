762ee50346b368d72dac438080dfb8d49ae6d7c9
yinm <yinm.cminor9@gmail.com>
2017-10-28 12:28:42 +0900

    Add multi dimensional array

--- ch4/a.c
+++ ch4/a.c
@@ -2,16 +2,15 @@
 
 main()
 {
+  int x, y;
   int a[2][3] = {
     {10, 20, 30},
     {40, 50, 60}
   };
-  a[0][2] = 0;
 
-  int x, y;
   for (y = 0; y < 2; y++) {
     for (x = 0; x < 3; x++) {
-      printf("a[%d][%d]=%d ", y, x, a[y][x]);
+      printf("a[%d][%d] = %d ", y, x, a[y][x]);
     }
     printf("\n");
   }
