5352a507c772ba1595e6aa7a323c6ba48c8d6738
yinm <yinm.cminor9@gmail.com>
2017-10-28 14:49:44 +0900

    Add trash program

--- ch5/a.c
+++ ch5/a.c
@@ -3,14 +3,32 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
+  char usage[] = "usage: trash <-d> filename\n";
+  int ret = 0;
+  char newfilename[256] = "";
 
-  if (argc <= 1)
+  if (argc <= 1) {
+    printf(usage);
     return 1;
-  if (strcmp(argv[1], "enum") == 0)
-    for (i = 0; i < argc; i++)
-      printf("argv[%d] : %s\n", i, argv[i]);
-  else if (strcmp(argv[1], "count") == 0)
-    printf("コマンドライン引数の数: %d\n", argc - 1);
-  return 0;
+  }
+  else if(strcmp(argv[1], "-d") == 0) {
+    if (argc <= 2) {
+      printf(usage);
+      return 2;
+    }
+    ret = remove(argv[2]);
+    if (ret == 0)
+      printf("ファイルを削除しました\n");
+    else
+      printf("ファイルを削除できませんでした\n");
+  }
+  else {
+    sprintf(newfilename, "%s.bak", argv[1]);
+    ret = rename(argv[1], newfilename);
+    if (ret == 0)
+      printf("ファイル名の最後に.bakを付加しました\n");
+    else
+      printf("ファイル名の変更ができませんでした\n");
+  }
 }
+
