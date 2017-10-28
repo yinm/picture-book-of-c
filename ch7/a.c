8d1dae51fcba2f5be5b67dc64544169fc20a4cbe
yinm <yinm.cminor9@gmail.com>
2017-10-28 22:14:05 +0900

    Add no typedef

--- ch7/a.c
+++ ch7/a.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-typedef struct _PROFILE {
+struct _PROFILE {
   char name[40];
   int age;
-} PROFILE;
+};
 
 int main()
 {
-  PROFILE prof[2] = {
+  struct _PROFILE prof[2] = {
     {"Maiko", 20},
     {"Naoki", 31}
   };
-
   int i;
   for (i = 0; i < 2; i++)
-    printf("%sさんは%d歳\n", prof[i].name, prof[i].age);
+    printf("%sさんは%d歳\n",
+        prof[i].name, prof[i].age);
 }
-
