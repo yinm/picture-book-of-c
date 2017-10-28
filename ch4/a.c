6404ea2d7c1a646016fe35d717bc6219a1a8d17f
yinm <yinm.cminor9@gmail.com>
2017-10-28 12:50:46 +0900

    Add null pointer2

--- ch4/a.c
+++ ch4/a.c
@@ -3,15 +3,15 @@
 
 main()
 {
-  char s[] = "I love cat.";
+  char s[] = "I love dog.";
   char c = 'd';
   char *p = NULL;
 
-  printf("文字列[%s]の中に文字[%c]", s, c);
-
+  printf("文字列 [%s] の中に文字 [%c] ", s, c);
   p = strchr(s, c);
   if (!p)
-    printf("はありません.\n");
+    printf("はありません\n");
   else
     printf("を発見しました.\n");
 }
+
