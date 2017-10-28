2eff8e86911915f7a6a8204fb7712411a1349118
yinm <yinm.cminor9@gmail.com>
2017-10-28 13:04:42 +0900

    Add manipulating memory

--- ch4/a.c
+++ ch4/a.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 
 main()
 {
-  int a[4] = {10, 20, 30, 40};
-  printf("配列a[3]の値は%d\n", *(a+3));
-  printf("配列a[0]の値に3を足すと%d\n", *a+3);
+  char *b;
+  char a[4] = {20, 40, 30, 10};
+
+  b = (char *)malloc(sizeof(char)*200);
+  if (!b)
+    return 1;
+
+  memcpy(b, a, sizeof(char)*200);
+  printf("%d %d %d %d\n", b[0], b[1], b[2], b[3]);
+  free(b);
 }
+
