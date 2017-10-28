22933ac1e515b272bd499ee0d8683dabb6592f18
yinm <yinm.cminor9@gmail.com>
2017-10-28 10:38:58 +0900

    Add continue

--- ch3/a.c
+++ ch3/a.c
@@ -5,10 +5,10 @@ main()
   int a = 1;
   int b = 1;
 
-  for (a = 0; a < 5; a++)
+  for (a = 1; a < 5; a++)
   {
-    if (a + b >= 3)
-      break;
+    if (a + b == 3)
+      continue;
     printf("%d+%d=%d\n", a, b, a + b);
   }
 }
