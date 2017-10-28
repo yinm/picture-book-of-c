465c6b0f0db3639347efd9979e6a2fa5042572be
yinm <yinm.cminor9@gmail.com>
2017-10-28 15:13:23 +0900

    Add recursion function

--- ch5/a.c
+++ ch5/a.c
@@ -1,42 +1,17 @@
 #include <stdio.h>
-#include <string.h>
 
-int wtoj(int, char *, int *);
+int kaijo(int);
 
 int main()
 {
-  int wyear = -1;
-  int jyear = 0;
-  char nengo[16];
-
-  printf("西暦->和暦の変換を行います。終了するには 0 を入力してください\n");
-  while (wyear != 0) {
-    printf("西暦を入力してください(1868 - 2050) : ");
-    scanf("%d", &wyear);
-    if (wtoj(wyear, nengo, &jyear) == 0)
-      printf("西暦%d年は、 %s%d年です\n", wyear, nengo, jyear);
-  }
-  return 0;
+  int n = kaijo(5);
+  printf("%d", n);
 }
 
-int wtoj(int wyear, char *nengo, int *jyear)
+int kaijo(int n)
 {
-  if (wyear >= 1868 && wyear <= 1911) {
-    strcpy(nengo, "明治");
-    *jyear = wyear - 1868 + 1;
-    return 0;
-  } else if (wyear >= 1912 && wyear <= 1925) {
-    strcpy(nengo, "大正");
-    *jyear = wyear - 1912 + 1;
-    return 0;
-  } else if (wyear >= 1926 && wyear <= 1988) {
-    strcpy(nengo, "昭和");
-    *jyear = wyear - 1926 + 1;
-    return 0;
-  } else if (wyear >= 1989 && wyear <= 2050) {
-    strcpy(nengo, "平成");
-    *jyear = wyear - 1989 + 1;
-    return 0;
-  }
-  return 1;
+  if (n == 0)
+    return 1;
+  else
+    return (n * kaijo(n - 1));
 }
