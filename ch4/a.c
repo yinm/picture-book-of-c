78355cb3766a81b0cc1662a4434bf5b868a7d2b2
yinm <yinm.cminor9@gmail.com>
2017-10-28 12:17:15 +0900

    Add multi dimensional array

--- ch4/a.c
+++ ch4/a.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 
 main()
 {
-  char a[] = "ABC";
-  char b[] = "ABCD";
-  int c = strcmp(a, b);
+  int a[2][3] = {
+    {10, 20, 30},
+    {40, 50, 60}
+  };
 
-  printf("%d", c);
+  a[0][2] = 0;
+  printf("%d\n", a[1][0]);
 }
-
