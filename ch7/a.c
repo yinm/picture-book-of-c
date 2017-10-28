c108068e4e82b396ebc30e146bd62cfe4fe5835b
yinm <yinm.cminor9@gmail.com>
2017-10-28 22:40:23 +0900

    Add calccal

--- ch7/a.c
+++ ch7/a.c
@@ -1,18 +1,76 @@
 #include <stdio.h>
+#include <string.h>
 
-struct _PROFILE {
+typedef struct _CALORIE {
   char name[40];
-  int age;
-};
+  float value;
+} CALORIE;
+
+int calregist(CALORIE *, int);
+float calcalc(CALORIE *, int);
 
 int main()
 {
-  struct _PROFILE prof[2] = {
-    {"Maiko", 20},
-    {"Naoki", 31}
+  CALORIE cal[500] = {
+    {"米飯", 150.0},
+    {"中華麺", 57.1},
+    {"そば", 133.3},
+    {"うどん", 100.0},
+    {"素麺", 133.3},
+    {"食パン", 250.0}
   };
+  int cal_num = 6;
+  int mode = 0;
+
+  printf("カロリー計算ツール\n");
+  while (1) {
+    printf("登録は 1 を、計算は 2 を、終了は 0 を入力してください: ");
+    scanf("%d", &mode);
+    if (mode == 0)
+      break;
+    else if (mode == 1)
+      cal_num = calregist(cal, cal_num);
+    else if (mode == 2)
+      printf("総カロリー:%6.2fkcal\n\n", calcalc(cal, cal_num));
+  }
+  return 0;
+}
+
+int calregist(CALORIE *pcal, int num)
+{
+  printf("食品名を入力してください: ");
+  scanf("%s", (pcal+num)->name);
+  printf("その食品のカロリーを入力してください[kcal/100g]: ");
+  scanf("%f", &((pcal + num)->value));
+  printf("登録しました。\n\n");
+  return num + 1;
+}
+
+float calcalc(CALORIE *pcal, int num)
+{
+  char name[40];
+  float gram;
+  float totalcal = 0.0;
   int i;
-  for (i = 0; i < 2; i++)
-    printf("%sさんは%d歳\n",
-        prof[i].name, prof[i].age);
+
+  printf("--食品名一覧------------\n");
+  for (i = 0; i < num; i++)
+    printf("%s\t", (pcal + i)->name);
+  printf("\n-------------------------------\n");;
+
+  while (1) {
+    printf("食品名 (endで計算): ");
+    scanf("%s", name);
+    if (strcmp(name, "end") == 0)
+      break;
+    printf("グラム数: ");
+    scanf("%f", &gram);
+    for (i = 0; i < num; i++) {
+      if (strcmp(name, (pcal + i)->name) == 0) {
+        totalcal += (pcal + i)->value + gram / 100.0;
+        break;
+      }
+    }
+  }
+  return totalcal;
 }
