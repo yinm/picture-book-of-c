e29d14323373633bf0b5c6a55c8ca454673f2627
yinm <yinm.cminor9@gmail.com>
2017-10-28 21:51:27 +0900

    Add struct pointer

--- ch7/a.c
+++ ch7/a.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-struct _point2d {
-  double x;
-  double y;
-} pt;
+struct _colorpoint2d {
+  double x, y;
+  int colorid;
+} cpt;
 
-void main()
+struct _colorpoint2d *ppt = &cpt;
+
+int main()
 {
-  pt.x = 30.0;
-  pt.y = 23.6;
-  printf("pt = (%4.1f, %4.1f)\n", pt.x, pt.y);
+  ppt->x = 2.4;
+  ppt->y = 3.2;
+  ppt->colorid = 1;
+  printf("(%3.1f, %3.1f) color=%d\n", ppt->x, ppt->y, ppt->colorid);
 }
+
