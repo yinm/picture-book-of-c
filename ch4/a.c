7dd39af60cf1e4c187d03b25b2df3aff17eafbe8
yinm <yinm.cminor9@gmail.com>
2017-10-29 21:44:39 +0900

    Add multi demension array

--- ch4/a.c
+++ ch4/a.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-  char s1[] = "cat";
-  char s2[] = "dog";
-  char s[20];
+  int x, y;
+  int a[2][3] = {
+    {10, 20, 30},
+    {40, 50, 60}
+  };
 
-  sprintf(s, "I love %s and %s.", s1, s2);
-  printf("[%s] の文字数は %d\n", s, strlen(s));
+  for (y = 0; y < 2; y++) {
+    for (x = 0; x < 3; x++)
+      printf("a[%d][%d] = %d ", y, x, a[y][x]);
+    printf("\n");
+  }
 }
