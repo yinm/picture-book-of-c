bb88f6d9569842614de7753845a6160dbaa1c15e
yinm <yinm.cminor9@gmail.com>
2017-10-29 22:04:49 +0900

    Add array and pointer

--- ch4/a.c
+++ ch4/a.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-  char s[] = "I love dog.";
-  char c = 'd';
-  char *p = NULL;
-
-  printf("文字列[%s]の中に文字[%c]", s, c);
-  p = strchr(s, c);
-
-  if (!p)
-    printf("はありません\n");
-  else
-    printf("を発見しました\n");
+  int a[4] = {10, 20, 30, 40};
+  printf("配列a[3]の値は%d\n", *(a+3));
+  printf("配列a[0]の値に3を足すと%d\n", *a+3);
 }
