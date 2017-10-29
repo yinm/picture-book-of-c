c8e5c50a98958792be675183cbf4380b4cd91dc2
yinm <yinm.cminor9@gmail.com>
2017-10-29 22:00:00 +0900

    Add null pointer

--- ch4/a.c
+++ ch4/a.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-  char x = 4;
-  char y;
-  char *p = &x;
-  y = *p;
+  char s[] = "I love cat.";
+  char c = 'd';
+  char *p = NULL;
 
-  printf("変数xの値は%dです\n", y);
+  printf("文字列[%s]の中に文字[%c]", s, c);
+  p = strchr(s, c);
+
+  if (!p)
+    printf("はありません\n");
+  else
+    printf("を発見しました\n");
 }
