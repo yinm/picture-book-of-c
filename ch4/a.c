09b396e75989404a15728fe81b1f141ee7759c30
yinm <yinm.cminor9@gmail.com>
2017-10-29 21:35:49 +0900

    Add array

--- ch4/a.c
+++ ch4/a.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
 
-main()
+int main()
 {
-  int mat[3][4] = {
-    {20, 42, 70, 34},
-    {67, 98, 37, 41},
-    {76, 99, 43, 65}
-  };
-  int i, j;
-  int sum_r;
-  int sum_c[4] = {0, 0, 0, 0};
-  int total = 0;
+  int i;
+  int a[] = {1, 2, 3, 4};
 
-  for (j = 0; j < 3; j++) {
-    sum_r = 0;
-    for (i = 0; i < 4; i++) {
-      printf("%4d", mat[j][i]);
-      sum_r += mat[j][i];
-      sum_c[i] += mat[j][i];
-    }
-    printf("| %4d\n", sum_r);
+  for (i = 3; i >= 0; i--) {
+    printf("%d ", a[i]);
   }
-
-  printf("----------------+-----\n");
-  for (i = 0; i < 4; i++) {
-    printf("%4d ", sum_c[i]);
-    total += sum_c[i];
-  }
-  printf("| %4d\n", total);
+  printf("\n");
 }
