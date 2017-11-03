9df6e29168194063d61cc1714773bcb79f8c1b7a
yinm <yinm.cminor9@gmail.com>
2017-11-03 19:09:07 +0900

    Add struct2

--- ch7/a.c
+++ ch7/a.c
@@ -3,11 +3,14 @@
 struct _point2d {
   double x;
   double y;
-} pt;
+};
 
 int main()
 {
-  pt.x = 30.0;
-  pt.y = 23.6;
+  struct _point2d pt = {
+    30.0,
+    23.6
+  };
+
   printf("pt = (%4.1f, %4.1f)\n", pt.x, pt.y);
 }
