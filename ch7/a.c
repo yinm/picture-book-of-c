68821fe752376774c5ae93dbaa3bf43efb6f642b
yinm <yinm.cminor9@gmail.com>
2017-11-03 19:11:53 +0900

    2nd

--- ch7/a.c
+++ ch7/a.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-struct _point2d {
+struct _colorpoint2d {
   double x;
   double y;
-};
+  int colorid;
+} cpt;
+struct _colorpoint2d *ppt = &cpt;
 
 int main()
 {
-  struct _point2d pt = {
-    30.0,
-    23.6
-  };
-
-  printf("pt = (%4.1f, %4.1f)\n", pt.x, pt.y);
+  ppt->x = 2.4;
+  ppt->y = 3.2;
+  ppt->colorid = 1;
+  printf("(%3.1f, %3.1f) color=%d\n", ppt->x, ppt->y, ppt->colorid);
 }
